bd420d143a1bda93a1c22ac3bfe310cdd118b233
Luis del Rio <luis@pluspeter.com>
2019-09-02 13:48:27 +0200

    [Feature] exercise 3 ch 8

---
+++ ch8/src/stats.rs
@@ -0,0 +1,30 @@
+pub mod stats {
+    use std::collections::HashMap;
+    pub fn calc(num_list: &mut Vec<i32>) -> (f32, i32, i32) {
+        num_list.sort();
+        let middle = num_list.len() / 2;
+        println!("{:?}", num_list);
+
+        let mut map = HashMap::new();
+        let mut mode = (0, 0);
+        let mut mean = 0.;
+        for num in num_list.iter_mut() {
+            let count = map.entry(*num).or_insert(0);
+            *count += 1;
+
+            if *count > mode.1 {
+                mode = (*num, *count)
+            }
+
+            mean = mean + *num as f32;
+        }
+        println!("{:?}", mode);
+
+        let mean = mean / num_list.len() as f32;
+        let median = num_list[middle];
+        let mode = mode.0;
+        println!("mean {:?}, median {:?}, mode {:?}", mean, median, mode);
+        (mean, median, mode)
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use ch3;
+use std::collections::HashMap;
+use std::io;
 
 fn main() {
     let c = ch3::f_to_c(212.);
@@ -8,14 +9,26 @@ fn main() {
     let fib = ch3::fib(8);
     println!("fib {}", fib);
 
-    // ch3::chris();
+    ch3::chris();
 
-    let mut nums = vec![3,8,5,7,2,35,2,6,2,8,9];
+    let mut num_list = vec![3, 8, 5, 7, 2, 35, 2, 6, 2, 8, 9];
 
-    ch8::stats(&mut nums);
+    ch8::stats(&mut num_list);
 
     let mut phrase = "hello you apple".to_string();
     let pl = ch8::pig_latin(&mut phrase);
     println!("{}", pl);
 
+    let mut departments = HashMap::new();
+
+    loop {
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        if command.len() > 1 {
+            ch8::parse_command(&command, &mut departments);
+        }
+    }
 }
